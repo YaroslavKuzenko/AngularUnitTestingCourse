bbe6f98e0c232f5190dfb0983b9cf7e14c9ba6a6
Yaroslav Kuzenko <yaroslavkuzenko02@gmail.com>
2024-07-02 12:16:46 +0300

    complete HeroService tests

--- src/app/hero.service.spec.ts
+++ src/app/hero.service.spec.ts
@@ -1,5 +1,5 @@
 import {HeroService} from "./hero.service";
-import {TestBed} from "@angular/core/testing";
+import {inject, TestBed} from "@angular/core/testing";
 import {NO_ERRORS_SCHEMA} from "@angular/core";
 import {MessageService} from "./message.service";
 import {HttpClientTestingModule, HttpTestingController} from "@angular/common/http/testing";
@@ -20,11 +20,18 @@ describe("Hero Service", () => {
       ]
     })
     httpTestingController = TestBed.inject(HttpTestingController);
-    let msgSvc = TestBed.inject(MessageService);
+    service = TestBed.inject(HeroService);
+
   })
 
   describe('getHero', () => {
-    
+    it('should call get with the correct URL', () =>{
+      service.getHero(4).subscribe();
+
+      const req = httpTestingController.expectOne('api/heroes/4');
+
+      req.flush({id: 4, name: 'Pes Patron', strength: 1231})
+    })
   })
 
 })
